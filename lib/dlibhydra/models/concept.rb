c95193839e2caa714e7fdbcdfc7a28ebbbfd54fd
Julie Allinson <j.allinson@gmail.com>
2016-03-06 00:17:47 +0000

    dlibhydra gem first commit

--- lib/dlibhydra/models/concept.rb
+++ lib/dlibhydra/models/concept.rb
@@ -1,13 +1,13 @@
 module Dlibhydra
   class Concept < ActiveFedora::Base
 
-    include Dlibhydra::RdfType #,Dlibhydra::Generic,Dlibhydra::SameAs,Dlibhydra::SkosLabels,Dlibhydra::DCTerms,Dlibhydra::AssignId,Dlibhydra::AssignRdfTypes
+    include Dlibhydra::RdfType,Dlibhydra::Generic,Dlibhydra::SameAs,Dlibhydra::SkosLabels,Dlibhydra::DCTerms,Dlibhydra::AssignId,Dlibhydra::AssignRdfTypes
 
     belongs_to :concept_scheme, class_name: 'ConceptScheme', predicate: ::RDF::SKOS.inScheme
 
     # Use only for Broader, Narrower will be added by default
-    #has_and_belongs_to_many :broader, class_name: 'Concept', predicate: ::RDF::SKOS.broader, inverse_of: :narrower
-    #has_and_belongs_to_many :narrower, class_name: 'Concept', predicate: ::RDF::SKOS.narrower, inverse_of: :broader
+    has_and_belongs_to_many :broader, class_name: 'Concept', predicate: ::RDF::SKOS.broader, inverse_of: :narrower
+    has_and_belongs_to_many :narrower, class_name: 'Concept', predicate: ::RDF::SKOS.narrower, inverse_of: :broader
 
     property :definition, predicate: ::RDF::SKOS.definition, multiple: false do |index|
       index.as :stored_searchable
