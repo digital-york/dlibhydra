139544021d52b551c4559f6a9c4847bbe8c57bad
Julie Allinson <julie.allinson@york.ac.uk>
2016-07-28 14:56:14 +0100

    fixing error in before_save

--- lib/dlibhydra/models/concerns/behaviours/add_labels.rb
+++ lib/dlibhydra/models/concerns/behaviours/add_labels.rb
@@ -10,18 +10,20 @@ module Dlibhydra
       before_save :map_labels
     end
 
+    private
     def map_labels
+      # if there's a preflabel, keep it
       if self.preflabel.class == String
-        if self.title.nil? # if there's a preflabel, keep it
+        if self.title = []
           self.title = [self.preflabel]
         else
           self.title << self.preflabel
         end
       elsif self.preflabel.nil? and self.title[0].class == String
         self.preflabel = self.title[0]
-      # if both preflabel and title are nil, validator will throw an error
-      end
+        # if both preflabel and title are nil, validator will throw an error
+        end
       self.rdfs_label = self.preflabel
-    end
+      end
   end
 end
