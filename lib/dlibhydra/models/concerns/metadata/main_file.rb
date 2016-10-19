6c680f917ecc9ba2198145d3cec0eec2622d9049
Julie Allinson <j.allinson@gmail.com>
2016-10-19 21:47:03 +0100

    testing main file

--- lib/dlibhydra/models/concerns/metadata/main_file.rb
+++ lib/dlibhydra/models/concerns/metadata/main_file.rb
@@ -7,7 +7,7 @@ module Dlibhydra
       # Use for the the link to the main file,
       # ie. the deposited thesis PDF and not any associated files
       # Although multiple links *could* be added, add only one.
-      has_and_belongs_to_many :mainFile, predicate: Dlibhydra::Vocab::Generic.hasMainFile
+      has_and_belongs_to_many :hasMainFile, predicate: Dlibhydra::Vocab::Generic.hasMainFile
                               # class_name: 'Dlibhydra::FileSet',
       # An alternative approach is a dedicated FileSet.
       # Currently Curation Concerns only allow a single FileSet.
@@ -16,9 +16,9 @@ module Dlibhydra
     
     # Add the FileSet to the members if it has not been added.
     def add_member
-      unless self.mainFile
-        unless self.members and self.members.include? self.mainFile.first
-          self.members << self.mainFile.first
+      unless self.hasMainFile
+        unless self.members and self.members.include? self.hasMainFile.first
+          self.members << self.hasMainFile.first
         end
       end
     end
