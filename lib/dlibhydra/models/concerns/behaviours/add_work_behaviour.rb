ca6e12aee7984ba7e5d42f4a54a4cffbd787234a
Julie Allinson <j.allinson@gmail.com>
2016-10-27 19:06:42 +0100

    removed NestedWorks from Work Behaviour

--- lib/dlibhydra/models/concerns/behaviours/add_work_behaviour.rb
+++ lib/dlibhydra/models/concerns/behaviours/add_work_behaviour.rb
@@ -1,23 +1,25 @@
 module Dlibhydra
   module AddWorkBehaviour
     extend ActiveSupport::Concern
-    include CurationConcerns::WorkBehavior
+    # include CurationConcerns::WorkBehavior
     # These are what WorkBehavior includes:
-    # include Hydra::Works::WorkBehavior
-    # include CurationConcerns::HumanReadableType
-    # include CurationConcerns::Noid
-    # include CurationConcerns::Permissions
-    # include CurationConcerns::Serializers
-    # include Hydra::WithDepositor
-    # include Solrizer::Common
-    # include CurationConcerns::HasRepresentative
-    # include CurationConcerns::WithFileSets
-    # include CurationConcerns::Naming
-    # include CurationConcerns::RequiredMetadata
-    # include CurationConcerns::InAdminSet
-    # include Hydra::AccessControls::Embargoable
-    # include GlobalID::Identification
-    # include CurationConcerns::NestedWorks # there was an issue with this, seems fixed?
-    # include CurationConcerns::Publishable
+    include Hydra::Works::WorkBehavior
+    include CurationConcerns::HumanReadableType
+    include CurationConcerns::Noid
+    include CurationConcerns::Permissions
+    include CurationConcerns::Serializers
+    include Hydra::WithDepositor
+    include Solrizer::Common
+    include CurationConcerns::HasRepresentative
+    include CurationConcerns::WithFileSets
+    include CurationConcerns::Naming
+    include CurationConcerns::RequiredMetadata
+    include CurationConcerns::InAdminSet
+    include Hydra::AccessControls::Embargoable
+    include GlobalID::Identification
+    # RuntimeError: Circular dependency detected while autoloading constant Thesis
+    # In CC app
+    # include CurationConcerns::NestedWorks
+    include CurationConcerns::Publishable
   end
 end
