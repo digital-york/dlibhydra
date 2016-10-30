58d676cb19648d0084e49452f76fe6d5df5f82b8
Julie Allinson <j.allinson@gmail.com>
2016-10-30 23:14:46 +0000

    trying a generator for dlibhydra authority files

--- lib/generators/dlibhydra/auths_generator.rb
+++ lib/generators/dlibhydra/auths_generator.rb
@@ -6,6 +6,19 @@ class Dlibhydra::AuthsGenerator < Rails::Generators::Base
   end
 
   def copy_qa_local
-    copy_file "qa_local.rb", "config/initializers/qa_local.rb"
+    file_path = 'config/initializers/qa_local.rb'
+    unless File.exist?(file_path)
+      copy_file "qa_local.rb", "config/initializers/qa_local.rb"
+    end
+    file_content = File.read(file_path)
+    Terms.constants.each do |term|
+      t = term.to_s
+      term_sting = "Qa::Authorities::Local.register_subauthority('#{t.gsub('Terms', '').humanize}s', 'Dlibhydra::Terms::#{t}')"
+      unless file_content.include?
+        inject_into_file file_path, after: '# include Terms' do
+          term_sting
+        end
+      end
+    end
   end
 end
