815273dfaad107a54a80b6840961972d891c1eb5
Julie Allinson <j.allinson@gmail.com>
2016-10-30 22:52:37 +0000

    trying a generator for dlibhydra authority files

--- lib/generators/dlibhydra/auths_generator.rb
+++ lib/generators/dlibhydra/auths_generator.rb
@@ -2,7 +2,7 @@ class Dlibhydra::AuthsGenerator < Rails::Generators::Base
   source_root File.expand_path('../templates', __FILE__)
 
   def copy_authorities
-    copy_file "authorities/licenses.yml", "config/authorities/licenses.yml"
+    copy_file "authorities", "config/authorities"
   end
 
   def copy_qa_local
