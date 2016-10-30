729bab1ee69018d32966ca81b418cb513538307e
Julie Allinson <j.allinson@gmail.com>
2016-10-30 18:45:37 +0000

    trying a generator for dlibhydra authority files

---
+++ lib/generators/dlibhydra/install_generator.rb
@@ -0,0 +1,7 @@
+class Dlibhydra::InstallGenerator < Rails::Generators::Base
+  source_root File.expand_path('../templates', __FILE__)
+
+  def copy_authorities
+    copy_file "authorities/licenses.yml", "config/authorities/licenses.yml"
+  end
+end
