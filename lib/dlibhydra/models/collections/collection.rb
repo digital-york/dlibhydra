071fb37c09db435d934063f092f0dbf3b44aecfa
Julie Allinson (tmp) <julieallinson@nas10-241-77-177.york.ac.uk>
2016-11-24 14:28:54 +0000

    collections indexer

--- lib/dlibhydra/models/collections/collection.rb
+++ lib/dlibhydra/models/collections/collection.rb
@@ -20,19 +20,12 @@ module Dlibhydra
       true
     end
 
-    # add the preflabel for _resource objects into solr
-    class TextIndexer < Hydra::PCDM::PCDMIndexer
-      def generate_solr_document
-        super.tap do |solr_doc|
-          # TODO add facetable
-          solr_doc['creator_value_tesim'] = object.creator_resource.collect { |x| x.preflabel }
-          solr_doc['subject_value_tesim'] = object.subject_resource.collect { |x| x.preflabel }
-        end
-      end
+    class CollectionIndexer < CurationConcerns::WorkIndexer # Hydra::PCDM::PCDMIndexer
+      include Dlibhydra::IndexesCollection
     end
 
     def self.indexer
-      TextIndexer
+      CollectionIndexer
     end
   end
 end
---
+++ lib/dlibhydra/models/concerns/behaviours/indexes_collection.rb
@@ -0,0 +1,47 @@
+module Dlibhydra
+  module IndexesThesis
+    extend ActiveSupport::Concern
+
+    included do
+      def generate_solr_document
+
+        # Add the object's HABM to 'authorities_tesim' or 'values_tesim' according to the convention being used.
+        #  'values_tesim' is used where the preflabel for the HABM resource is stored in solr only.
+        #  'authorities_tesim' is used where the preflabel for the HABM resource is stored in the object too.
+        #   These are used by the authorities in it's update_usages method to update all references if the
+        #   authority term changes.
+        values_to_index = ['creator','subject', 'publisher']
+
+        super.tap do |solr_doc|
+          solr_doc['values_tesim'] = []
+
+          values_to_index.each do |v|
+            method = "#{v}_resource"
+            solr_doc["#{v}_value_alt_tesim"] = []
+            prefs = object.send(method).collect { |x| x.preflabel }
+            solr_doc["#{v}_value_tesim"] = prefs # stored searchable
+            solr_doc["#{v}_value_ssim"] = prefs
+            solr_doc["#{v}_value_sim"] = prefs # facetable TODO check this
+
+            object.send(method).each do |a|
+              solr_doc["#{v}_value_alt_tesim"] += a.altlabel.to_a
+            end
+
+            solr_doc['values_tesim'] += object.send(method).collect { |x| x.id }
+          end
+
+          # creators
+          solr_doc['creator_tesim'] = object.creator.collect { |x| x }
+          solr_doc['creator_ssim'] = object.creator.collect { |x| x }
+          solr_doc['creator_sim'] = object.creator.collect { |x| x }
+          solr_doc['creator_tesim'] += solr_doc["creator_value_tesim"].collect { |x| x }
+          solr_doc['creator_ssim'] += solr_doc["creator_value_ssim"].collect { |x| x }
+          solr_doc['creator_sim'] += solr_doc["creator_value_ssim"].collect { |x| x }
+          solr_doc['creator_resource_tesim'] = object.creator_resource_ids.collect { |x| x }
+
+        end
+      end
+    end
+
+  end
+end
