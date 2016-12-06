d23d152dce74d51097ba9f7722f4083b539f800f
Frank <frank.feng@york.ac.uk>
2016-12-06 15:58:00 +0000

    update

--- lib/dlibhydra/models/filesets/file_set.rb
+++ lib/dlibhydra/models/filesets/file_set.rb
@@ -1,15 +1,21 @@
 module Dlibhydra
   # dlibhydra fileset
   class FileSet < ActiveFedora::Base
+    # TODO check CC Collection Behaviour
     include Hydra::Works::FileSetBehavior,
             Dlibhydra::AddLabels,
-			Dlibhydra::Derivatives #,
-            #CurationConcerns::Noid
-  
+            CurationConcerns::Noid
+
     def authority?
       false
     end
     def work?
+      false
+    end
+    def file_set?
+      true
+    end
+    def collection?
       false
     end
 
