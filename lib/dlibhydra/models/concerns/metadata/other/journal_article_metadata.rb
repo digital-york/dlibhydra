8f7452e708b9f93f636268288e5ee72eef70860c
Frank <frank.feng@york.ac.uk>
2016-12-22 16:16:07 +0000

    added add_journal_article_values

--- lib/dlibhydra/models/concerns/metadata/other/journal_article_metadata.rb
+++ lib/dlibhydra/models/concerns/metadata/other/journal_article_metadata.rb
@@ -4,10 +4,13 @@ module Dlibhydra
     extend ActiveSupport::Concern
 
     included do
-      has_and_belongs_to_many :journal,
+      has_and_belongs_to_many :journal_resource,
                             class_name: 'Dlibhydra::Journal',
                             predicate: ::RDF::URI.new('https://vocab.org/frbr/core#partOf')
       
+      def add_journal_article_values
+        self.journal = journal_resource.collect { |x| x.preflabel }
+      end
     end
   end
 end
