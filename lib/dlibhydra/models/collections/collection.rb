6e347e1ba388ebfc83ae9e1ae7a3d2ae23a4f6fb
Frank <frank.feng@york.ac.uk>
2017-01-19 15:19:17 +0000

    update

--- lib/dlibhydra/models/collections/collection.rb
+++ lib/dlibhydra/models/collections/collection.rb
@@ -1,7 +1,7 @@
 module Dlibhydra
   class Collection < ActiveFedora::Base
     # TODO check CC Collection Behaviour
-    include Hydra::Works::CollectionBehavior, #Hyrax::CollectionBehavior,
+    include Hyrax::CollectionBehavior, # Hydra::Works::CollectionBehavior,
             Dlibhydra::AddLabels,
             Dlibhydra::AddDcDescriptive,
             Dlibhydra::DcRights,
