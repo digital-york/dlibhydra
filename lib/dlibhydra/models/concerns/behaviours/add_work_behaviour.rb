e0e85179bea0a64facc351b4b54800a76e3545ec
Frank <frank.feng@york.ac.uk>
2017-01-19 14:41:29 +0000

    change add_work_behaviour back and use Hydra::Works::CollectionBehavior in collection

--- lib/dlibhydra/models/concerns/behaviours/add_work_behaviour.rb
+++ lib/dlibhydra/models/concerns/behaviours/add_work_behaviour.rb
@@ -4,19 +4,19 @@ module Dlibhydra
     # include Hyrax::WorkBehavior
     # These are what WorkBehavior includes:
     include Hydra::Works::WorkBehavior
-    #include Hyrax::HumanReadableType
+    include Hyrax::HumanReadableType
     include Hyrax::Noid
-    #include Hyrax::Permissions
-    #include Hyrax::Serializers
-    #include Hydra::WithDepositor
-    #include Solrizer::Common
+    include Hyrax::Permissions
+    include Hyrax::Serializers
+    include Hydra::WithDepositor
+    include Solrizer::Common
     include Hyrax::HasRepresentative
-    #include Hyrax::WithFileSets
-    #include Hyrax::Naming
+    include Hyrax::WithFileSets
+    include Hyrax::Naming
     include Hyrax::RequiredMetadata
     include Hyrax::InAdminSet
-    #include Hydra::AccessControls::Embargoable
-    #include GlobalID::Identification
+    include Hydra::AccessControls::Embargoable
+    include GlobalID::Identification
     include Hyrax::Suppressible
     # RuntimeError: Circular dependency detected while autoloading constant Thesis
     # In CC app
