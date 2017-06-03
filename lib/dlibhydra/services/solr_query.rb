322199a35f007bf349e372cb8496a56181223999
Fergus McGlynn <fergus.a.mcglynn@york.ac.uk>
2017-06-03 13:09:52 +0000

    added comment explaining the change

--- lib/dlibhydra/services/solr_query.rb
+++ lib/dlibhydra/services/solr_query.rb
@@ -7,6 +7,7 @@ module Dlibhydra
   class SolrQuery
 
     def solr_query(q, fl='id', rows=10, sort='', start=0)
+      # use the solr instance already loaded in ActiveFedora rather than digging it out of config/solr.yml (which might contain ERB)
       ActiveFedora::SolrService.instance.conn.get('select', :params => {
           :q => q,
           :fl => fl,
