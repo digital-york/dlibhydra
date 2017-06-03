0b873d799fbd29aae9f9071ea2134da8d515bbee
Fergus McGlynn <fergus.a.mcglynn@york.ac.uk>
2017-06-03 13:01:15 +0000

    call slightly different 'get' method

--- lib/dlibhydra/services/solr_query.rb
+++ lib/dlibhydra/services/solr_query.rb
@@ -7,7 +7,7 @@ module Dlibhydra
   class SolrQuery
 
     def solr_query(q, fl='id', rows=10, sort='', start=0)
-      ActiveFedora::SolrService.get('select', :params => {
+      ActiveFedora::SolrService.instance.conn.get('select', :params => {
           :q => q,
           :fl => fl,
           :rows => rows,
