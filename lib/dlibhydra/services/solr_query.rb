c97b846230545648afb82b51172860ca376fac76
Fergus McGlynn <fergus.a.mcglynn@york.ac.uk>
2017-06-03 12:56:38 +0000

    enclosed solr query in parentheses as argument to 'get'

--- lib/dlibhydra/services/solr_query.rb
+++ lib/dlibhydra/services/solr_query.rb
@@ -7,13 +7,13 @@ module Dlibhydra
   class SolrQuery
 
     def solr_query(q, fl='id', rows=10, sort='', start=0)
-      ActiveFedora::SolrService.get 'select', :params => {
+      ActiveFedora::SolrService.get('select', :params => {
           :q => q,
           :fl => fl,
           :rows => rows,
           :sort => sort,
           :start => start
-      }
+      })
     end
   end
 end
